e4f284890cbc0e69e55ff7a65592048a8ad7064c
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-08 14:11:49 +0000

    :feat/routes for navigation accross the page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth-routing.module').then((m) => m.authRoutes),
+  },
+
+  {
+    path: 'link',
+    loadChildren: () =>
+      import('./link/link-routing.module').then((m) => m.linkRoutes),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+];
