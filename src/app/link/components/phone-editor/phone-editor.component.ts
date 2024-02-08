a02d2576230b5e8332722b9ce6d2e55e027216b8
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-08 14:20:26 +0000

    :feat/ phone user interface

---
+++ src/app/link/components/phone-editor/phone-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-editor.component.html',
+  styleUrl: './phone-editor.component.css'
+})
+export class PhoneEditorComponent {
+
+}
