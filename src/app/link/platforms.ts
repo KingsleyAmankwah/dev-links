764b406373274f89dd034019fc3afec200c776bc
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-09 16:05:45 +0000

    feat/ Platform details

---
+++ src/app/link/platforms.ts
@@ -0,0 +1,74 @@
+export const PLATFORMS = [
+  {
+    id: 1,
+    name: 'GitHub',
+    ImageURL: '../assets/images/icon-github-gray.svg',
+  },
+  {
+    id: 2,
+    name: 'Frontend Mentor',
+    ImageURL: '../assets/images/icon-frontend-mentor-gray.svg',
+  },
+  {
+    id: 3,
+    name: 'Twitter',
+    ImageURL: '../assets/images/icon-twitter-gray.svg',
+  },
+  {
+    id: 4,
+    name: 'LinkedIn',
+    ImageURL: '../assets/images/icon-linkedin-gray.svg',
+  },
+  {
+    id: 5,
+    name: 'YouTube',
+    ImageURL: '../assets/images/icon-youtube-gray.svg',
+  },
+  {
+    id: 6,
+    name: 'Facebook',
+    ImageURL: '../assets/images/icon-facebook-gray.svg',
+  },
+
+  {
+    id: 7,
+    name: 'Codepen',
+    ImageURL: '../assets/images/icon-codepen-gray.svg',
+  },
+  {
+    id: 8,
+    name: 'Twitch',
+    ImageURL: '../assets/images/icon-twitch-gray.svg',
+  },
+  {
+    id: 9,
+    name: 'Devto',
+    ImageURL: '../assets/images/icon-devto-gray.svg',
+  },
+
+  {
+    id: 10,
+    name: 'Codewars',
+    ImageURL: '../assets/images/icon-codewars-gray.svg',
+  },
+  {
+    id: 11,
+    name: 'Hashnode',
+    ImageURL: '../assets/images/icon-hashnode-gray.svg',
+  },
+  {
+    id: 12,
+    name: 'FreeCodeCamp',
+    ImageURL: '../assets/images/icon-freecodecamp-gray.svg',
+  },
+  {
+    id: 13,
+    name: 'GitLab',
+    ImageURL: '../assets/images/icon-gitlab-gray.svg',
+  },
+  {
+    id: 14,
+    name: 'FreeCodeCamp',
+    ImageURL: '../assets/images/icon-stack-overflow-gray.svg',
+  },
+];
