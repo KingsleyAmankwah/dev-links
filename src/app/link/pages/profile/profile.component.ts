e35f6115596d32cc9b0b050f943f8ed2c5de504d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-09 09:40:23 +0000

    :feat/ profile component

--- src/app/link/pages/profile/profile.component.ts
+++ src/app/link/pages/profile/profile.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { PhoneEditorComponent } from '../../components/phone-editor/phone-editor.component';
+import { userDetails } from '../../../auth/interfaces';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [PhoneEditorComponent],
+  imports: [PhoneEditorComponent, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  userData!: userDetails;
   imageUrl = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  profileForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    email: ['', [Validators.email]],
+    profileImage: [''],
+  });
 
   onSelectedFile(e: any) {
     if (e.target.files) {
@@ -20,4 +37,23 @@ export class ProfileComponent {
       };
     }
   }
+
+  onSave() {}
+
+  // onSave() {
+  //   if (this.profileForm.valid) {
+  //     const userData: userDetails = {
+  //       firstName: this.profileForm.value.firstName!,
+  //       lastName: this.profileForm.value.lastName!,
+  //       email: this.profileForm.value.email!,
+  //       profileImage: this.imageUrl,
+  //     };
+  //     // this.userData = this.profileForm.value;
+  //     localStorage.setItem('userDetails', JSON.stringify(userData));
+  //     console.log('User details:', userData);
+  //     this.saveDetails();
+  //   } else {
+  //     console.log('Form is not valid');
+  //   }
+  // }
 }
