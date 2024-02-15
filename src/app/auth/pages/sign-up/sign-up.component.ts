895385b64892dac0032d44a07f54cd77dd7033b4
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-15 16:47:44 +0000

    feat/(app): Implement sign-up functionality

--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputComponent } from '../../components/input/input.component';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [InputComponent, RouterLink],
+  imports: [InputComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  isLoading = false;
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+
+  registerForm = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.isLoading = true;
+      const { name, email, password } = this.registerForm.value;
+      this.authService.signUp(
+        email as string,
+        password as string,
+        name as string
+      );
+
+      this.isLoading = false;
+    }
+  }
+}
