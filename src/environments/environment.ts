dd1de72a6bd643ff3c46a9ba5e2dd914bf65948d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-15 17:16:01 +0000

    ...

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7ckUoEvixCZIznC6j-lycKC76VXmZr6w',
+    authDomain: 'dev-links-bf2b1.firebaseapp.com',
+    projectId: 'dev-links-bf2b1',
+    storageBucket: 'dev-links-bf2b1.appspot.com',
+    messagingSenderId: '379857548190',
+    appId: '1:379857548190:web:c32a54103f7f15507edc8f',
+  },
+};
