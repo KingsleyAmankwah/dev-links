24e5c295617cb933fb83a4add88810eb9cfd855d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-15 16:47:28 +0000

    feat/(app): Implement sign-in functionality

--- src/app/auth/pages/sign-in/sign-in.component.ts
+++ src/app/auth/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputComponent } from '../../components/input/input.component';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [InputComponent, RouterLink],
+  imports: [InputComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  isLoading = false;
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email as string, password as string);
+      this.isLoading = false;
+    }
+  }
+}
