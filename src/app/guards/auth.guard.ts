19feeddc18d982500b66829ac473b57189cab045
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-16 18:24:19 +0000

    feat/(app): Implement auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,20 @@
 import { CanActivateFn } from '@angular/router';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const auth = getAuth();
-  return !!auth.currentUser; // return true if the user is logged in, else false
+  return new Observable<boolean>((observer) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (user) => {
+        observer.next(!!user); // emit true if the user is logged in, false otherwise
+        observer.complete();
+      },
+      (error) => {
+        observer.error(error);
+      }
+    );
+    return unsubscribe; // return the unsubscribe function to clean up the subscription
+  });
 };
