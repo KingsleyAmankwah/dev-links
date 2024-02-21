f59da8e1873508f0044f0cdcfa31507acae41ac6
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-21 15:21:39 +0000

    feat(app): Implement user copylink

--- src/app/link/pages/preview/preview.component.ts
+++ src/app/link/pages/preview/preview.component.ts
@@ -9,4 +9,18 @@ import { PhoneEditorComponent } from '../../components/phone-editor/phone-editor
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.css',
 })
-export class PreviewComponent {}
+export class PreviewComponent {
+
+  
+  copyLink() {
+    const url = window.location.href;
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        alert('Link copied to clipboard');
+      })
+      .catch((err) => {
+        console.error('Failed to copy: ', err);
+      });
+  }
+}
