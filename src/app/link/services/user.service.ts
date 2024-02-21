e4d7cb08fadab8e5ab6f4b4a7f6f95a48eee251c
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-21 15:13:25 +0000

    feat/(app): user service updated to fetch user details

--- src/app/link/services/user.service.ts
+++ src/app/link/services/user.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { userDetails } from '../../auth/interfaces';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private userDetailsSubject = new BehaviorSubject<userDetails | null>(null);
-
-  setUserDetails(userDetails: userDetails): void {
-    this.userDetailsSubject.next(userDetails);
-  }
-
-  getUserDetails(): Observable<userDetails | null> {
-    return this.userDetailsSubject.asObservable();
+  public userSubject = new BehaviorSubject<userDetails | null>(null);
+  user = this.userSubject.asObservable();
+  constructor() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        user.reload().then(() => {
+          this.userSubject.next({
+            name: user.displayName || '',
+            email: user.email || '',
+            profileImage: user.photoURL || '',
+          });
+        });
+      } else {
+        this.userSubject.next(null);
+      }
+    });
   }
 }
