c2183e81658b36ba5c3b04b594b2762cccbb8523
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-21 15:20:21 +0000

    feat/(app): implement user details display

--- src/app/link/components/phone-editor/phone-editor.component.ts
+++ src/app/link/components/phone-editor/phone-editor.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { getAuth } from 'firebase/auth';
+import { UserService } from '../../services/user.service';
+import { Subscription } from 'rxjs';
+import { userDetails } from '../../../auth/interfaces';
 
 @Component({
   selector: 'app-phone-editor',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
   templateUrl: './phone-editor.component.html',
   styleUrl: './phone-editor.component.css',
 })
-export class PhoneEditorComponent {}
+export class PhoneEditorComponent {
+  name: string | null = null;
+  email: string | null = null;
+  imageUrl: string | null = null;
+  private subscription: Subscription | undefined;
+  userService = inject(UserService);
+
+  ngOnInit() {
+    this.subscription = this.userService.user.subscribe((user) => {
+      if (user) {
+        this.name = user.name;
+        this.email = user.email;
+        this.imageUrl = user.profileImage;
+      } else {
+        this.name = null;
+        this.email = null;
+        this.imageUrl = null;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+}
