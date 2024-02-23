bb068a36417f41363906517170114f18c60029fa
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-23 16:57:46 +0000

    refactor/(app): form coponent

--- src/app/link/components/link-form/link-form.component.ts
+++ src/app/link/components/link-form/link-form.component.ts
@@ -2,17 +2,26 @@ import { Component, EventEmitter, Input, NgZone, Output } from '@angular/core';
 import { formLinks } from '../../interfaces';
 import { CdkDropListGroup, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { PLATFORMS } from '../../platforms';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-link-form',
   standalone: true,
-  imports: [CdkDropListGroup, CdkDrag, CdkDropList],
+  imports: [
+    CdkDropListGroup,
+    CdkDrag,
+    CdkDropList,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './link-form.component.html',
   styleUrl: './link-form.component.css',
 })
 export class LinkFormComponent {
   @Input() link!: formLinks;
   @Input() index!: number;
+  @Input() linkFormGroup!: FormGroup;
 
   platforms = PLATFORMS;
   showPlatforms = false;
@@ -30,6 +39,7 @@ export class LinkFormComponent {
 
   onPlatformClicked(platform: { id: number; name: string; ImageURL: string }) {
     this.selectedPlatform = platform;
-    this.showPlatforms = false;
+    this.linkFormGroup.get('platform')?.setValue(platform);
+    console.log(this.linkFormGroup.get('platform')?.value);
   }
 }
