50473b86cc5ad8d1dbc02f90afe46a1e038f0bc4
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-23 16:56:26 +0000

    feat/(app): Implmement save user links to firebase

--- src/app/link/services/user.service.ts
+++ src/app/link/services/user.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { userDetails } from '../../auth/interfaces';
 import { BehaviorSubject } from 'rxjs';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  CollectionReference,
+  addDoc,
+  collection,
+  getFirestore,
+} from 'firebase/firestore';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +30,23 @@ export class UserService {
       } else {
         this.userSubject.next(null);
       }
+    });
+  }
+
+  private db = getFirestore();
+
+  async saveLinks(links: any[]): Promise<void> {
+    const linksCollection: CollectionReference = collection(this.db, 'links');
+
+    for (const link of links) {
+      await addDoc(linksCollection, link);
+    }
+
+    Swal.fire({
+      title: 'Links Saved!',
+      icon: 'success',
+      timer: 3000,
+      showConfirmButton: false,
     });
   }
 }
