0a6aacd841376c68cb5b25b7c2b6e902aca43731
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-24 16:52:16 +0000

    preview component

--- src/app/link/pages/preview/preview.component.ts
+++ src/app/link/pages/preview/preview.component.ts
@@ -1,26 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PhoneEditorComponent } from '../../components/phone-editor/phone-editor.component';
+import { AuthService } from '../../../auth/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [RouterLink, PhoneEditorComponent],
+  imports: [RouterLink, PhoneEditorComponent, CommonModule],
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.css',
 })
 export class PreviewComponent {
+  userId!: string;
+  authService = inject(AuthService);
+  route = inject(ActivatedRoute);
 
-  
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.userId = params['userId'];
+    });
+  }
+
   copyLink() {
-    const url = window.location.href;
-    navigator.clipboard
-      .writeText(url)
-      .then(() => {
-        alert('Link copied to clipboard');
-      })
-      .catch((err) => {
-        console.error('Failed to copy: ', err);
-      });
+    const userId = this.userId || this.authService.getUserId();
+    if (userId) {
+      const profileLink = `/link/preview/${userId}`;
+      const url = window.location.origin + profileLink;
+
+      // Copy the link to the clipboard
+      navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          alert('Link copied to clipboard');
+        })
+        .catch((err) => {
+          console.error('Failed to copy: ', err);
+        });
+    } else {
+      console.error('User is not authenticated. Unable to copy link.');
+    }
   }
 }
+
