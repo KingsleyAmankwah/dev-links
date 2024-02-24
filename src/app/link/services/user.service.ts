85d231f7939565512e5667cebbc758361dced797
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-24 16:51:06 +0000

    user service

--- src/app/link/services/user.service.ts
+++ src/app/link/services/user.service.ts
@@ -8,7 +8,10 @@ import {
   DocumentReference,
   addDoc,
   collection,
+  getDocs,
   getFirestore,
+  query,
+  where,
 } from 'firebase/firestore';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../auth/services/auth.service';
@@ -18,6 +21,7 @@ import { AuthService } from '../../auth/services/auth.service';
 })
 export class UserService {
   public userSubject = new BehaviorSubject<userDetails | null>(null);
+  private db = getFirestore();
   user = this.userSubject.asObservable();
 
   authService = inject(AuthService);
@@ -28,18 +32,19 @@ export class UserService {
       if (user) {
         user.reload().then(() => {
           this.userSubject.next({
+            id: user.uid,
             name: user.displayName || '',
             email: user.email || '',
             profileImage: user.photoURL || '',
           });
+          // Fetch and provide social media links
+          this.fetchSocialMediaLinks(user.uid);
         });
       } else {
         this.userSubject.next(null);
       }
     });
   }
-
-  private db = getFirestore();
 
   async saveLinks(links: any[]): Promise<void> {
     const user = this.authService.getCurrentUser();
@@ -74,6 +79,39 @@ export class UserService {
       });
     } else {
       console.error('User is not authenticated. Unable to save links.');
+    }
+  }
+
+  async fetchSocialMediaLinks(userId: string): Promise<void> {
+    const linksCollection: CollectionReference = collection(this.db, 'links');
+
+    const userLinksQuery = query(
+      linksCollection,
+      where('userRef', '==', userId)
+    );
+    console.log('Fetching social media links for user: ', userId);
+    try {
+      const linksSnapshot = await getDocs(userLinksQuery);
+      console.log('Links snapshot: ', linksSnapshot);
+      const socialMediaLinks = linksSnapshot.docs.map((doc) => doc.data());
+      console.log(socialMediaLinks);
+
+      // Retrieve existing user details
+      const existingUserDetails = this.userSubject.getValue() as userDetails;
+
+      if (existingUserDetails) {
+        this.userSubject.next({
+          ...existingUserDetails,
+          id: userId,
+          socialMediaLinks: socialMediaLinks,
+        });
+      } else {
+        console.error(
+          'Existing user details are null. Unable to update social media links.'
+        );
+      }
+    } catch (error) {
+      console.error('Error fetching social media links: ', error);
     }
   }
 }
