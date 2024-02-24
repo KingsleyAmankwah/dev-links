9b163f170128a055002acd2d0d465092ee7d70fc
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-24 16:49:48 +0000

    auth interface

--- src/app/auth/interfaces/index.ts
+++ src/app/auth/interfaces/index.ts
@@ -1,5 +1,9 @@
+import { DocumentData } from "firebase/firestore";
+
 export interface userDetails {
+  id: string;
   name: string;
   email: string;
   profileImage: string;
+  socialMediaLinks?: DocumentData[];
 }
