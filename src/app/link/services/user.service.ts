17546cf66c581e619e017787cf81f4253faabacf
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-24 15:13:37 +0000

    refactor/(app): user service refactoerd

--- src/app/link/services/user.service.ts
+++ src/app/link/services/user.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { userDetails } from '../../auth/interfaces';
 import { BehaviorSubject } from 'rxjs';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import {
   CollectionReference,
+  DocumentData,
+  DocumentReference,
   addDoc,
   collection,
   getFirestore,
 } from 'firebase/firestore';
 import Swal from 'sweetalert2';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +19,9 @@ import Swal from 'sweetalert2';
 export class UserService {
   public userSubject = new BehaviorSubject<userDetails | null>(null);
   user = this.userSubject.asObservable();
+
+  authService = inject(AuthService);
+
   constructor() {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
@@ -36,17 +42,38 @@ export class UserService {
   private db = getFirestore();
 
   async saveLinks(links: any[]): Promise<void> {
-    const linksCollection: CollectionReference = collection(this.db, 'links');
+    const user = this.authService.getCurrentUser();
 
-    for (const link of links) {
-      await addDoc(linksCollection, link);
+    if (user) {
+      const linksCollection: CollectionReference = collection(this.db, 'links');
+
+      for (const link of links) {
+        // Associate each link with the authenticated user
+        const linkData = {
+          ...link,
+          userRef: user.uid,
+        };
+
+        try {
+          // Use addDoc to add a document to the 'links' collection
+          const docRef: DocumentReference<DocumentData> = await addDoc(
+            linksCollection,
+            linkData
+          );
+          console.log('Document written with ID: ', docRef.id);
+        } catch (error) {
+          console.error('Error adding document: ', error);
+        }
+      }
+
+      Swal.fire({
+        title: 'Links Saved!',
+        icon: 'success',
+        timer: 3000,
+        showConfirmButton: false,
+      });
+    } else {
+      console.error('User is not authenticated. Unable to save links.');
     }
-
-    Swal.fire({
-      title: 'Links Saved!',
-      icon: 'success',
-      timer: 3000,
-      showConfirmButton: false,
-    });
   }
 }
